f5e912824ceebcbb2e8329d322db8eb319eae02d
ginsenglee <thecynicdog0328@gmail.com>
2023-06-11 12:28:47 +0900

    pre otp features dropped

--- src/test/java/practice/ch3demo2/SecurityTests.java
+++ src/test/java/practice/ch3demo2/SecurityTests.java
@@ -8,37 +8,31 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import practice.ch3demo2.config.SecurityConfig;
 import practice.ch3demo2.config.SpringDataJpaConfig;
-import practice.ch3demo2.controller.HelloController;
+import practice.ch3demo2.controller.AuthController;
 import practice.ch3demo2.entity.Authority;
-import practice.ch3demo2.entity.Token;
 import practice.ch3demo2.model.SecurityUser;
 import practice.ch3demo2.entity.User;
-import practice.ch3demo2.repository.TokenRepository;
+import practice.ch3demo2.repository.OtpRepository;
 import practice.ch3demo2.repository.UserRepository;
-import practice.ch3demo2.service.CsrfTokenService;
 import practice.ch3demo2.service.JpaUserDetailsManager;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc @Import(HelloController.class)
+@AutoConfigureMockMvc @Import(AuthController.class)
 @ContextConfiguration(classes = { SpringDataJpaConfig.class, SecurityConfig.class})
 public class SecurityTests {
 
@@ -57,68 +51,71 @@ public class SecurityTests {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Mock
-    TokenRepository tokenRepository;
+    @Autowired
+    OtpRepository otpRepository;
+
+//    @Mock
+//    TokenRepository tokenRepository;
 
     @Test
     public void databaseConnectionTest() {
-
-        JpaUserDetailsManager jpaUserDetailsManager = new JpaUserDetailsManager(userRepository, passwordEncoder);
-
-        SecurityUser userDetails = new SecurityUser(
-                new User("john", "1234", List.of(
-                        new Authority("john","read"),
-                        new Authority("john","write")))
-        );
-
-        jpaUserDetailsManager.createUser(userDetails);
-
-        SecurityUser found = (SecurityUser) jpaUserDetailsManager.loadUserByUsername(userDetails.getUsername());
-
-        assertAll(
-                () -> assertEquals("john", found.getUsername()),
-                () -> assertTrue(passwordEncoder.matches("1234", found.getPassword())),
-                () -> assertEquals(2, found.getAuthorities().size())
-        );
+//
+//        JpaUserDetailsManager jpaUserDetailsManager = new JpaUserDetailsManager(userRepository, passwordEncoder, otpRepository);
+//
+//        SecurityUser userDetails = new SecurityUser(
+//                new User("john", "1234", List.of(
+//                        new Authority("john","read"),
+//                        new Authority("john","write")))
+//        );
+//
+//        jpaUserDetailsManager.createUser(userDetails);
+//
+//        SecurityUser found = (SecurityUser) jpaUserDetailsManager.loadUserByUsername(userDetails.getUsername());
+//
+//        assertAll(
+//                () -> assertEquals("john", found.getUsername()),
+//                () -> assertTrue(passwordEncoder.matches("1234", found.getPassword())),
+//                () -> assertEquals(2, found.getAuthorities().size())
+//        );
     }
 
     @Test
     public void loggingWithAuthenticationTest() throws Exception {
-
-        JpaUserDetailsManager jpaUserDetailsManager = new JpaUserDetailsManager(userRepository, passwordEncoder);
-
-        SecurityUser userDetails = new SecurityUser(
-                new User("bill", "1234", List.of(
-                        new Authority("bill","read"),
-                        new Authority("bill","write")))
-        );
-
-        jpaUserDetailsManager.createUser(userDetails);
-
-        mvc.perform(formLogin("/login").user("bill").password("1234"))
-                .andExpect(authenticated());
+//
+//        JpaUserDetailsManager jpaUserDetailsManager = new JpaUserDetailsManager(userRepository, passwordEncoder, otpRepository);
+//
+//        SecurityUser userDetails = new SecurityUser(
+//                new User("bill", "1234", List.of(
+//                        new Authority("bill","read"),
+//                        new Authority("bill","write")))
+//        );
+//
+//        jpaUserDetailsManager.createUser(userDetails);
+//
+//        mvc.perform(formLogin("/login").user("bill").password("1234"))
+//                .andExpect(authenticated());
     }
 
     @Test
     public void csrfServiceTest() {
 
-        CsrfTokenService csrfTokenService = new CsrfTokenService(tokenRepository);
-
-        String sessionId = "sessionId for testing";
-
-        Token token = new Token();
-        token.setToken("token for testing");
-        token.setSessionId(sessionId);
-        token.setUsername("username for testing");
-
-        when(httpServletRequest.getSession()).thenReturn(mock(HttpSession.class));
-        when(httpServletRequest.getSession().getId()).thenReturn(sessionId);
-        when(tokenRepository.findTokenBySessionId(sessionId)).thenReturn(Optional.of(token));
-
-        CsrfToken csrfToken = csrfTokenService.loadToken(httpServletRequest);
-
-        assertEquals("X-CSRF-TOKEN", csrfToken.getHeaderName());
-        assertEquals("_csrf", csrfToken.getParameterName());
-        assertEquals("token for testing", csrfToken.getToken());
+//        CsrfTokenService csrfTokenService = new CsrfTokenService(tokenRepository);
+//
+//        String sessionId = "sessionId for testing";
+//
+//        Token token = new Token();
+//        token.setToken("token for testing");
+//        token.setSessionId(sessionId);
+//        token.setUsername("username for testing");
+//
+//        when(httpServletRequest.getSession()).thenReturn(mock(HttpSession.class));
+//        when(httpServletRequest.getSession().getId()).thenReturn(sessionId);
+//        when(tokenRepository.findTokenBySessionId(sessionId)).thenReturn(Optional.of(token));
+//
+//        CsrfToken csrfToken = csrfTokenService.loadToken(httpServletRequest);
+//
+//        assertEquals("X-CSRF-TOKEN", csrfToken.getHeaderName());
+//        assertEquals("_csrf", csrfToken.getParameterName());
+//        assertEquals("token for testing", csrfToken.getToken());
     }
 }
